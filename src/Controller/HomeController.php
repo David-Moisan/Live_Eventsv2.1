59fec937311021689bf4ca56109449f5f9a93676
David-Moisan <dmoisan.23@gmail.com>
2021-04-11 16:51:43 +0200

    Config: DB and first page

---
+++ src/Controller/HomeController.php
@@ -0,0 +1,20 @@
+<?php
+
+namespace App\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+
+class HomeController extends AbstractController
+{
+    /**
+     * @Route("/", name="home")
+     *
+     * @return Response
+     */
+    public function home(): Response
+    {
+        return $this->render('home_page.html.twig');
+    }
+}
