277443482336e67fd6e693d442ada4d9274f0e56
David-Moisan <dmoisan.23@gmail.com>
2021-04-19 19:06:59 +0200

    Update: Partenaires render miss a Responsive view

--- src/Controller/PartenaireController.php
+++ src/Controller/PartenaireController.php
@@ -2,6 +2,7 @@
 
 namespace App\Controller;
 
+use App\Repository\CategoriePartenairesRepository;
 use App\Repository\PartenairesRepository;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
@@ -12,12 +13,14 @@ class PartenaireController extends AbstractController
     /**
      * @Route("/partenaire", name="partenaire.index")
      */
-    public function index(PartenairesRepository $repo): Response
+    public function index(PartenairesRepository $repo, CategoriePartenairesRepository $categorieRepo): Response
     {
         $partenaires = $repo->findAll();
+        $categorie = $categorieRepo->findAll();
 
         return $this->render('partenaire/index.html.twig', [
             'partenaires' => $partenaires,
+            'categorie' => $categorie,
         ]);
     }
 }
