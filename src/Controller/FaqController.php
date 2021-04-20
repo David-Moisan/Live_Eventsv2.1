7c8418e88490acc5f4b27c3e713a051f7c1c97b0
David-Moisan <dmoisan.23@gmail.com>
2021-04-20 16:25:12 +0200

    Update: Routes

---
+++ src/Controller/FaqController.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace App\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+
+class FaqController extends AbstractController
+{
+    /**
+     * @Route("/faq", name="faq")
+     */
+    public function index(): Response
+    {
+        return $this->render('pages/faq.html.twig');
+    }
+}
