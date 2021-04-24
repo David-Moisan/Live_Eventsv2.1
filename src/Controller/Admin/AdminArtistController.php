dc92d320863a562736e8c48cfdad8b9cdf03250a
David-Moisan <dmoisan.23@gmail.com>
2021-04-24 15:33:52 +0200

    Create: Artiste CRUD

--- src/Controller/Admin/AdminArtistController.php
+++ src/Controller/Admin/AdminArtistController.php
@@ -2,9 +2,12 @@
 
 namespace App\Controller\Admin;
 
+use App\Entity\Artist;
+use App\Form\ArtistType;
 use App\Repository\ArtistRepository;
 use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
@@ -37,22 +40,79 @@ class AdminArtistController extends AbstractController
 
     /**======================================================== */
 
-    public function edit()
+    /**
+     * New.
+     *
+     * @Route("/admin/artiste/create", name="admin.artiste.new")
+     *
+     * @return void
+     */
+    public function new(Request $request)
     {
-        // code...
+        $artiste = new Artist();
+
+        $form = $this->createForm(ArtistType::class, $artiste);
+        $form->handleRequest($request);
+
+        if ($form->isSubmitted() && $form->isValid()) {
+            $this->em->persist($artiste);
+            $this->em->flush();
+            $this->addFlash('success', 'Artiste Créer avec succès');
+
+            return $this->redirectToRoute('admin.artiste.index');
+        }
+
+        return $this->render('/admin/artiste/new.html.twig', [
+            'artiste' => $artiste,
+            'form' => $form->createView(),
+        ]);
     }
 
     /**======================================================== */
 
-    public function new()
+    /**
+     * Edit.
+     *
+     * @Route("/admin/artiste/{id}", name="admin.artiste.edit", methods="GET|POST")
+     *
+     * @return void
+     */
+    public function edit(Request $request, Artist $artiste)
     {
-        // code...
+        $form = $this->createForm(ArtistType::class, $artiste);
+        $form->handleRequest($request);
+
+        if ($form->isSubmitted() && $form->isValid()) {
+            $this->em->persist($artiste);
+            $this->em->flush();
+            $this->addFlash('success', 'Artiste Modifié avec succès');
+
+            return $this->redirectToRoute('admin.artiste.index');
+        }
+
+        return $this->render('/admin/artiste/edit.html.twig', [
+            'artiste' => $artiste,
+            'form' => $form->createView(),
+        ]);
     }
 
     /**======================================================== */
 
-    public function delete()
+    /**
+     * Delete.
+     *
+     * @Route("/admin/artiste/{id}", name="admin.artiste.delete", methods="DELETE")
+     *
+     * @return void
+     */
+    public function delete(Artist $artiste, Request $request)
     {
-        // code...
+        if ($this->isCsrfTokenValid('delete'.$artiste->getId(), $request->get('_token'))) {
+            $this->em->remove($artiste);
+            $this->em->flush();
+            $this->addFlash('success', 'Artiste supprimé avec succès');
+        }
+
+        return $this->redirectToRoute('admin.artiste.index');
     }
 }
---
+++ src/Form/ArtistType.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace App\Form;
+
+use App\Entity\Artist;
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\OptionsResolver\OptionsResolver;
+
+class ArtistType extends AbstractType
+{
+    public function buildForm(FormBuilderInterface $builder, array $options)
+    {
+        $builder
+            ->add('name')
+            ->add('hours')
+            ->add('scene')
+            ->add('id_program')
+        ;
+    }
+
+    public function configureOptions(OptionsResolver $resolver)
+    {
+        $resolver->setDefaults([
+            'data_class' => Artist::class,
+        ]);
+    }
+}
